3e48ddb8c251ab6a202257c2da8dba31735abe53
Christian Brotherson <108023689+cbrotherson@users.noreply.github.com>
2025-06-02 15:32:08 -0600

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -5,7 +5,7 @@ set -e
 # DOWNLOAD
 tsc="$HOME/tsc/library/node/tsc"
 download_url="https://github.com/TrustedSmartChain/tsc/releases/download/v1.0.0-alpha1/tsc_1.8.2"
-curl $download_url -o $tsc --create-dirs
+curl -L $download_url -o $tsc --create-dirs
 chmod +x $tsc
 
 # ADD ALIAS
